588e041975134a349821b953584642bc11f5bc00
Surendra Nath <surendra.nath@phm.co.uk>
2019-12-16 09:28:54 +0000

    Tidy Up the methods

--- src/domain/Mars.Rover.Models/Rover.cs
+++ src/domain/Mars.Rover.Models/Rover.cs
@@ -6,19 +6,20 @@ namespace Mars.Rover.Models
     public class Rover
     {
         private readonly string _direction = "N";
+
         public string Execute(string commands)
         {
-            var direction=_direction;
+            var direction = _direction;
             foreach (var command in commands.ToCharArray().ToList())
             {
-                if(command.Equals('R'))
+                if (command.Equals('R'))
                     direction = rotateRight(direction);
                 if (command.Equals('L'))
                 {
                     direction = rotateLeft(direction);
                 }
 
-                
+
 
             }
 
@@ -30,21 +31,13 @@ namespace Mars.Rover.Models
         private string rotateLeft(string direction)
         {
             if (direction.Equals("N"))
-            {
                 return "W";
-            }
             if (direction.Equals("E"))
-            {
                 return "N";
-            }
             if (direction.Equals("S"))
-            {
                 return "E";
-            }
             if (direction.Equals("W"))
-            {
                 return "S";
-            }
 
             throw new Exception();
         }
@@ -52,23 +45,16 @@ namespace Mars.Rover.Models
         private string rotateRight(string direction)
         {
             if (direction.Equals("N"))
-            {
                 return "E";
-            }
             if (direction.Equals("E"))
-            {
                 return "S";
-            }
             if (direction.Equals("S"))
-            {
                 return "W";
-            }
             if (direction.Equals("W"))
-            {
                 return "N";
-            }
 
             throw new Exception();
         }
+
     }
 }
